1302f1020d23092ab7b24189d2f830d342b8e949
cainiaobang <lqb_seu@163.com>
2017-11-19 11:19:13 +0800

    new files

---
+++ src/main/java/org/smart4j/framework/helper/DataBaseHelper.java
@@ -0,0 +1,212 @@
+package org.smart4j.framework.helper;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.smart4j.framework.Util.CollectionUtil;
+import org.smart4j.framework.Util.PropsUtil;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+public class DataBaseHelper {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DataBaseHelper.class);
+    private static final String DRIVER;
+    private static final String URL;
+    private static final String USERNAME;
+    private static final String PASSWORD;
+    private static final ThreadLocal<Connection> CONNECTION_HOLDER = new ThreadLocal<Connection>();
+    private static final BasicDataSource DATA_SOURCE;
+
+    static {
+        Properties conf = PropsUtil.loadProps("config.properties");
+        DRIVER = conf.getProperty("jdbc.driver");
+        URL = conf.getProperty("jdbc.url");
+        USERNAME = conf.getProperty("jdbc.username");
+        PASSWORD = conf.getProperty("jdbc.password");
+        DATA_SOURCE = new BasicDataSource();
+        DATA_SOURCE.setDriverClassName(DRIVER);
+        DATA_SOURCE.setUrl(URL);
+        DATA_SOURCE.setUsername(USERNAME);
+        DATA_SOURCE.setPassword(PASSWORD);
+
+    }
+
+    public static Connection getConnection() {
+        Connection conn = CONNECTION_HOLDER.get();
+        if (conn == null) {
+            try {
+                conn = DATA_SOURCE.getConnection();
+
+            } catch (SQLException e) {
+                LOGGER.error("execute sql failure", e);
+                throw new RuntimeException(e);
+            } finally {
+                CONNECTION_HOLDER.set(conn);
+            }
+        }
+        return conn;
+    }
+
+
+    public static final QueryRunner QUERY_RUNNER = new QueryRunner();
+
+    public static <T> List<T> queryEntityList(Class<T> entityClass,
+                                              String sql, Object... params) {
+        List<T> entityList;
+        Connection conn;
+        try {
+            conn = getConnection();
+            entityList = QUERY_RUNNER.query(conn, sql, new BeanListHandler<T>(entityClass), params);
+        } catch (SQLException e) {
+            LOGGER.error("query entity list failure", e);
+            throw new RuntimeException(e);
+        } finally {
+        }
+        return entityList;
+    }
+
+    public static <T> T queryEntity(Class<T> entityClass, String sql, Object... params) {
+        T entity;
+        try {
+            Connection conn = getConnection();
+            entity = QUERY_RUNNER.query(conn, sql, new BeanHandler<T>(entityClass), params);
+        } catch (SQLException e) {
+            LOGGER.error("query entity failure", e);
+            throw new RuntimeException(e);
+        }
+        return entity;
+    }
+
+    public static List<Map<String, Object>> executeQuery(String sql, Object... params) {
+        List<Map<String, Object>> result;
+        try {
+            Connection conn = getConnection();
+            result = QUERY_RUNNER.query(conn, sql, new MapListHandler(), params);
+        } catch (Exception e) {
+            LOGGER.error("execute query failure", e);
+            throw new RuntimeException(e);
+        } finally {
+        }
+        return result;
+    }
+
+    public static int executeUpdate(String sql, Object... params) {
+        int rows = 0;
+        try {
+            Connection conn = getConnection();
+            rows = QUERY_RUNNER.update(conn, sql, params);
+        } catch (Exception e) {
+            LOGGER.error("execute query failure", e);
+            throw new RuntimeException(e);
+        } finally {
+        }
+        return rows;
+    }
+
+    public static <T> boolean insertEntity(Class<T> entityClass, Map<String, Object> fieldMap) {
+        if (CollectionUtil.isEmpty(fieldMap)) {
+            LOGGER.error("can not insert entity: fieldMap is empty");
+            return false;
+        }
+        String sql = "insert into " + getTableName(entityClass);
+        StringBuilder columns = new StringBuilder("(");
+        StringBuilder values = new StringBuilder("(");
+        for (String fieldName : fieldMap.keySet()) {
+            columns.append(fieldName).append(",");
+            values.append("?,");
+        }
+        columns.replace(columns.lastIndexOf(","), columns.length(), ")");
+        values.replace(values.lastIndexOf(","), values.length(), ")");
+        sql += columns + "  values" + values;
+        Object[] params = fieldMap.values().toArray();
+        return executeUpdate(sql, params) == 1;
+
+    }
+
+
+    public static <T> boolean updateEntity(Class<T> entityClass, long id,
+                                           Map<String, Object> fieldMap) {
+        if (CollectionUtil.isEmpty(fieldMap)) {
+            LOGGER.error("can not insert entity: fieldMap is empty");
+            return false;
+        }
+        String sql = "update " + getTableName(entityClass) + " set ";
+        StringBuilder columns = new StringBuilder();
+        for (String fieldName : fieldMap.keySet()) {
+            columns.append(fieldName).append("=?, ");
+        }
+        sql += columns.substring(0, columns.indexOf(",")) + " where id = ?";
+        List<Object> paramList = new ArrayList<Object>();
+        paramList.addAll(fieldMap.values());
+        paramList.add(id);
+        Object[] params = paramList.toArray();
+        return executeUpdate(sql, params) == 1;
+    }
+
+    public static <T> boolean delateEntity(Class<T> entityClass, long id) {
+        String sql = "delete from " + getTableName(entityClass) + " where id =?";
+        return executeUpdate(sql, id) == 1;
+    }
+
+
+    private static String getTableName(Class<?> entityClass) {
+        return entityClass.getSimpleName();
+    }
+
+
+    public static void beginTransaction() {
+        Connection conn = getConnection();
+        if (conn != null) {
+            try {
+                conn.setAutoCommit(false);
+            } catch (SQLException e) {
+                LOGGER.error("begin transaction failure", e);
+                throw new RuntimeException(e);
+            } finally {
+                CONNECTION_HOLDER.set(conn);
+            }
+        }
+    }
+
+    public static void commitTransaction() {
+        Connection conn = getConnection();
+        if (conn != null) {
+            try {
+                conn.commit();
+                conn.close();
+            } catch (SQLException e) {
+                LOGGER.error("commit transaction failure", e);
+                throw new RuntimeException(e);
+            } finally {
+                CONNECTION_HOLDER.remove();
+            }
+        }
+    }
+
+    public static void rollbackTransaction() {
+        Connection conn = getConnection();
+        if (conn != null) {
+            try {
+                conn.rollback();
+                conn.close();
+                ;
+            } catch (SQLException e) {
+                LOGGER.error("rollback transaction failure", e);
+                throw new RuntimeException(e);
+            } finally {
+                CONNECTION_HOLDER.remove();
+            }
+        }
+    }
+
+}
+
